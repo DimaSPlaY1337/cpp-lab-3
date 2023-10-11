3a9e47306e70670b56b10f54c6a9df43a6450939
Dima Galitsyn <dimasplay1337@gmail.com>
2023-10-11 11:10:24 +0300

    Update code

--- cpp-lab-3.cpp
+++ cpp-lab-3.cpp
@@ -2,38 +2,26 @@
 
 using namespace std;
 
-int n, m;
-
 int recursion(int i, int n, int Masha, int Petya, long* massive)
 {
-	cout << " i = " << i << "; " <<
-		"n = " << n << "; " <<
-		"Masha = " << Masha << "; " <<
-		"Petya = " << Petya << ". " << endl;
 	if (i == n)
 	{
-		if (Masha < Petya)
-		{
-			cout << "Petya - Masha = " << Petya - Masha << endl;
-			return Petya - Masha;
-		}
-		else
-		{
-			cout << "Masha - Petya = " << Masha - Petya << endl;
-			return Masha - Petya;
-		}
+		return abs(Petya - Masha);
 	}
 	return min(recursion(i + 1, n, Masha + massive[i], Petya, massive), recursion(i + 1, n, Masha, Petya + massive[i], massive));
 }
 
 int main()
 {
+	int n, m;
+
 	cout << "Enter count of cupes(1<=...<=32): ";
 	cin >> n;
+
 	long* mas = new long[n];
 	for (int i = 0; i < n; i++)
 	{
-		cout << i << "Enter gramm for each cupe: ";
+		cout << i << " Enter gramm for each cupe: ";
 		cin >> mas[i];
 	}
 
